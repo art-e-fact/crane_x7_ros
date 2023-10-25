88aae4bb7392f483fa1989986292104cd094314c
decarabas <codrea.eric@gmail.com>
2023-10-24 19:54:56 -0500

    Added method of recording x and y ee poses

--- crane_x7_tests/crane_x7_tests/arm_pose_subscriber.py
+++ crane_x7_tests/crane_x7_tests/arm_pose_subscriber.py
@@ -3,27 +3,36 @@ from rclpy.node import Node
 
 from geometry_msgs.msg import PoseArray
 
+
 class JointStateSubscriber(Node):
     def __init__(self):
         super().__init__("arm_pose_state_subscriber")
         self.subscription = self.create_subscription(
-            PoseArray, "/world/default/dynamic_pose/info", self.arm_pose_callback, 10
+            PoseArray,
+            "/world/default/dynamic_pose/info",
+            self.gripper_pose_callback,
+            10,
         )
 
         self.subscription
+        self.gripper_x_pose_list = []
+        self.gripper_y_pose_list = []
 
-    def arm_pose_callback(self, msg):
-        # rounded_joint_values = []
-        # decimal_place = 2
-        # for i in range(len(msg.position)):
-        #     rounded_joint = round(msg.position[i], decimal_place)
+    def gripper_pose_callback(self, msg):
+        """
+        Subscriber callback for getting the pose of the Crane_X7 gripper link x and y positions
+        """
+        gripper_index_value = -3
 
-        #     if rounded_joint == -0.0:
-        #         rounded_joint = 0.0
+        gripper_pose = msg.poses[gripper_index_value]
+        gripper_x_pose = gripper_pose.position.x
+        gripper_y_pose = gripper_pose.position.y
 
-        #     rounded_joint_values.append(rounded_joint)
+        self.gripper_x_pose_list.append(gripper_x_pose)
+        self.gripper_y_pose_list.append(gripper_y_pose)
 
-        self.get_logger().info('arm_pose: "%s" \n' % msg)
+        # print(gripper_x_pose)
+        # self.get_logger().info('arm_pose: "%s" \n' % msg.poses[gripper_index_value])
 
 
 def main(args=None):
